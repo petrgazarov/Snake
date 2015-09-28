9d09ae12fcd9bf15952c60c12554eeb7f8372a66
Petr Gazarov <petrgazarov@gmail.com>
2015-09-28 17:45:38 -0400

    write View#handleKeyEvent

--- js/snakeView.js
+++ js/snakeView.js
@@ -6,5 +6,28 @@
   var View = SGame.View = function(element) {
     this.$el = element;
     this.board = new SGame.Board();
+
+    this.$el.keydown(function(e) {
+      this.handleKeyEvent(e);
+    }
   }
-}
+
+  View.prototype.handleKeyEvent = function() {
+    switch(e.which) {
+        case 37: this.snake.turn("W");
+        break;
+
+        case 38: this.snake.turn("N");
+        break;
+
+        case 39: this.snake.turn("E");
+        break;
+
+        case 40: this.snake.turn("S");
+        break;
+
+        default: return;
+    }
+    e.preventDefault();
+  }
+});
