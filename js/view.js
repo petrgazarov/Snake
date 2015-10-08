4de33d404132f2a99707c6b7ba6b8c5a3f7d7e55
Petr Gazarov <petrgazarov@gmail.com>
2015-10-07 20:50:35 -0400

    add rounds and next round modals and add styling

--- js/view.js
+++ js/view.js
@@ -8,12 +8,11 @@
     this.gamesPlayed = 0;
     this.$el = $(selector);
     this.board = new SGame.Board();
+    this.nextRoundModal = false;
 
     $('html').keydown(function(e) {
       this.handleKeyEvent(e);
     }.bind(this));
-
-    this.interval = 130;
   }
 
   View.prototype.handleKeyEvent = function(e) {
@@ -59,6 +58,8 @@
 
   View.prototype.startGame = function() {
     if (!this.intervalId) {
+      this.interval = 140;
+      this.round = 0;
       this.setUpForNewGame();
       this.board = new SGame.Board();
       this.firstStep();
@@ -70,7 +71,8 @@
       window.clearInterval(this.intervalId);
       this.intervalId = null;
       this.borderRed();
-      $('#board').append($('<h3>').addClass('game-status').text('GAME OVER'));
+      $('#board').append($('<h3>').addClass('game-status')
+                 .text('GAME OVER').css('font-size', '36px'));
   }
 
   View.prototype.step = function() {
@@ -78,14 +80,17 @@
       this.gameOver();
       return;
     }
+
     if (this.board.snakeWillEatApple()) {
       this.updateScore(false);
       this.borderGreen();
       window.setTimeout(this.borderBlack.bind(this), 250);
+      this.incrementRoundIfHit20();
     }
     this.board.moveSnake();
     var grid = this.board.render();
     this.$el.html(this.board.render());
+    if (this.nextRoundModal) { this.showNextRoundModal(); }
 
     this.stepping = false;
   }
@@ -116,6 +121,31 @@
     $('.games-played').text(this.stringify(this.gamesPlayed));
     this.gamesPlayed++;
     this.updateScore(true);
+  }
+
+  View.prototype.incrementRoundIfHit20 = function() {
+    if (this.currentScore % 20 === 0 && this.currentScore !== 0) {
+      this.round++;
+      this.resetInterval(30);
+      this.showNextRoundModal(true);
+    }
+  }
+
+  View.prototype.resetInterval = function(unitsFaster) {
+    window.clearInterval(this.intervalId);
+    this.intervalId = window.setInterval(this.step.bind(this), this.interval -= unitsFaster);
+  }
+
+  View.prototype.showNextRoundModal = function(initial) {
+    if (initial) {
+      this.nextRoundModal = true;
+      window.setTimeout(function() {
+        this.nextRoundModal = false;
+      }.bind(this), 5000);
+    }
+
+    $('#board').append($('<h3>').addClass('game-status')
+               .text('TURBO '+ this.round).css('font-size', '36px'));
   }
 
   View.prototype.stringify = function(number) {
